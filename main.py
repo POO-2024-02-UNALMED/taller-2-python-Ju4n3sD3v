1e16ef1cd2ba8c30a3c06d7bfa29a23380423b9e
Ju4n3sD3v <juanherrera@unal.edu.co>
2024-11-10 12:56:16 -0500

    Update main.py
    Se hicieron cambios a los métodos de cada clase

--- main.py
+++ main.py
@@ -4,22 +4,21 @@ class Auto:
     def __init__(self, modelo, precio, Asientos, marca, Motor, registro) -> None:
         self.modelo = modelo   
         self.precio = precio
-        self.Asientos = Asientos
+        self.asientos = Asientos
         self.marca = marca
-        self.Motor = Motor
+        self.motor = Motor
         self.registro = registro   
 
     def cantidadAsientos(self) -> int:
-        for asiento in self.Asientos:
-            if type(asiento) == None:
-                self.Asientos.pop(asiento)
-        nroAsientos = len(self.Asientos)
+        nroAsientos = len(self.asientos) - self.asientos.count(None)
+
         return nroAsientos
     
     def verificarIntegridad(self) -> str:
-        registros = set(self.registro, self.Motor.registro)
-        for asiento in self.Asientos:
-            registros.add(asiento.registro)
+        registros = {self.registro, self.motor.registro}
+        for asiento in self.asientos:
+            if asiento != None:
+                registros.add(asiento.registro)
 
         if len(registros) > 1:
             return 'Las piezas no son originales'
@@ -32,10 +31,10 @@ class Motor:
         self.tipo = tipo
         self.registro = registro
 
-    def cambiarRegistros(self, nuevoRegistro: int):
+    def cambiarRegistro(self, nuevoRegistro):
         self.registro = nuevoRegistro
 
-    def asignarTipo(self, nuevoTipo: str):
+    def asignarTipo(self, nuevoTipo):
         if nuevoTipo.lower() in ['electrico', 'gasolina']:
             self.tipo = nuevoTipo
 
@@ -46,7 +45,7 @@ class Asiento:
         self.precio = precio
         self.registro = registro
 
-    def cambiarColor(self, nuevoColor: str) -> None:
+    def cambiarColor(self, nuevoColor) -> None:
         if nuevoColor.lower() in ['rojo', 'verde', 'amarillo', 'negro', 'blanco']:
             self.color = nuevoColor
 
